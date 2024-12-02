0da1b4e12153641af39e8e5810097fb69b4391be
louie-horn <louie-horn05@gmail.com>
2024-12-02 11:25:46 +0000

    day one part one solved

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -7,6 +7,38 @@ public class Day01 : BaseDay
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
+        string[] pairs = _input.Split("\n");
+
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+        
+        for (int i = 0; i < (pairs.Length - 1); i++)
+        {
+            string[] split_pair = pairs[i].Trim().Split("   ");
+            
+            leftList.Add(int.Parse(split_pair[0]));
+            rightList.Add(int.Parse(split_pair[1]));
+        }
+        
+        leftList.Sort();
+        rightList.Sort();
+        int distance = 0;
+        
+        for (int i = 0; i < leftList.Count; i++)
+        {
+            if (leftList[i] != rightList[i])
+            {
+                if (leftList[i] < rightList[i])
+                {
+                    distance += rightList[i] - leftList[i];
+                }
+                else if (leftList[i] > rightList[i])
+                {
+                    distance += leftList[i] - rightList[i];
+                }
+            }
+        }
+        Console.WriteLine(distance);
     }
 
     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
